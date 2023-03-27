01cf488afaaf3ec507380c66c31f42f8f2ff630c
db-gb <dbarnas90@gmail.com>
2023-03-27 12:10:22 -0700

    Create a python library

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+    name='streamsketchlib',
+    packages=find_packages(include=['streamsketchlib']),
+    version='0.1.0',
+    description='Python library consisting of various streaming algorithms',
+    author='Hoa Vu and Daniel Barnas',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==7.2.2'],
+    test_suite='tests',
+)
