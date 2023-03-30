9756f1fb8e3f59b162fa128adbb78f8999c13718
Hoa Vu <hoavu.tr@gmail.com>
2023-03-30 13:04:11 -0700

    Add F2 sketch. Add F2 and F0 tests. Clean up F0 sketch.

--- streamsketchlib/f0_estimate.py
+++ streamsketchlib/f0_estimate.py
@@ -1,111 +1,90 @@
 import mmh3
 import math
-import random
-import sys
-import os
 import bisect
 from bisect import bisect_left
-import time
 import statistics
-from pympler.asizeof import asizeof
-
-def BinarySearch(a, x):
-    i = bisect_left(a, x)
-    if i != len(a) and a[i] == x:
-        return i
-    else:
-        return -1
 
 
 class F0Estimate:
-    """ Loglog algorithm: memory efficient data structure to estimate
-    the number of distinct elements in streams """
-
-    def __init__(self, epsilon=0.01, delta=0.01, c=1, hash_type="mmh3", seed=42):
-        """ epsilon: relative error, delta: failure probability
-        for each hash function h, maintain the smallest hash value H
-        z = median of depth averages of width smallest hash values
-        return 1/z - 1 """
-
-        self.depth = c*int(math.log(1/delta, 2))
+    """ 
+    Loglog algorithm: memory efficient data structure to estimate
+    the number of distinct elements in streams 
+    """
+    def __init__(self, epsilon=0.01, delta=0.01, c=2, hash_type="mmh3", seed=42):
+        """ epsilon: relative error
+        delta: failure probability
+        This is the first algorithm of [BJKST02]
+        """
         self.epsilon = epsilon
         self.hash_type = hash_type
+        self.max_32_int = pow(2, 32)-1
 
-        # t ~ 2/eps^2
-        self.t = c*int(math.pow(1/self.epsilon, 2))
+        # width ~ c/eps^2 and depth ~ c log(1/delta)
+        self.width = c*int(math.pow(1/self.epsilon, 2))
+        self.depth = c*int(math.log(1/delta, 2))
+        self.seeds = [seed*i for i in range(self.depth)]
 
         # data structure to store the smallest t hash values
-        self.smallest_hashes = [[] for i in range(self.depth)]
+        self.table = [[] for i in range(self.depth)]
 
         # a list to store all distinct values if F0 < t
-        self.small_list = set()
+        self.naive_lst = set()
 
-        self.max_32_int = pow(2, 32)-1
-        self.seeds = [0 for _ in range(self.depth)]
-
-        for i in range(self.depth):
-            self.seeds[i] = i*i*seed
-
+    def _binary_search(self, a, x):
+        i = bisect_left(a, x)
+        if i != len(a) and a[i] == x:
+            return i
+        else:
+            return -1
 
     def _hash(self, token, seed):
         """ Compute the hash of a token. """
         if self.hash_type == "mmh3":
             return mmh3.hash(token, seed, signed=False)/self.max_32_int
 
-
     def insert(self, token):
         """ Insert a token into the sketch. Token must be byte-like objects. """
-        if len(self.small_list) < self.t:
-            self.small_list.add(token)
+        if len(self.naive_lst) < self.width:
+            self.naive_lst.add(token)
 
         for i in range(self.depth):
             hash_value = self._hash(token, self.seeds[i])
-            j = BinarySearch(self.smallest_hashes[i], hash_value)
+            j = self._binary_search(self.table[i], hash_value)
 
             if j == -1:
-                if len(self.smallest_hashes[i]) < self.t:
-                    bisect.insort(self.smallest_hashes[i], hash_value)
-
-                elif self.smallest_hashes[i][self.t-1] > hash_value:
-                    bisect.insort(self.smallest_hashes[i], hash_value)
-                    self.smallest_hashes[i].pop()
+                if len(self.table[i]) < self.width:
+                    bisect.insort(self.table[i], hash_value)
+                elif self.table[i][self.width-1] > hash_value:
+                    bisect.insort(self.table[i], hash_value)
+                    self.table[i].pop()
 
     def merge(self, S):
         """ Merge self and another sketch S that must have same seeds """
         # merge the small lists
-        for x in S.small_list:
-            if len(self.small_list) < self.t:
-                self.small_list.add(x)
+        for x in S.naive_lst:
+            if len(self.naive_lst) < self.width:
+                self.naive_lst.add(x)
             else:
                 break
 
         # merge the smallest hash values
         for i in range(self.depth):
-            for x in S.smallest_hashes[i]:
-                j = BinarySearch(self.smallest_hashes[i], x)
+            for x in S.table[i]:
+                j = self._binary_search(self.table[i], x)
                 if j  == -1:
-                    if len(self.smallest_hashes[i]) < self.t:
-                        bisect.insort(self.smallest_hashes[i], x)
-                    elif self.smallest_hashes[i][self.t-1] > x:
-                        bisect.insort(self.smallest_hashes[i], x)
-                        self.smallest_hashes[i].pop()
-
+                    if len(self.table[i]) < self.width:
+                        bisect.insort(self.table[i], x)
+                    elif self.table[i][self.width-1] > x:
+                        bisect.insort(self.table[i], x)
+                        self.table[i].pop()
 
     def estimator(self):
         """ Return the estimate for the number of distinct
         elements inserted so far """
         #start_time = time.time()
-
-        if len(self.small_list) < self.t:
-            result = len(self.small_list)
-            #print(f"F0 algorithm estimation time: {time.time() - start_time}")
+        if len(self.naive_lst) < self.width:
+            result = len(self.naive_lst)
             return result
-
-        est = []
-        for i in range(self.depth):
-            l = len(self.smallest_hashes[i])
-            est.append(int(self.t/self.smallest_hashes[i][l-1]))
-
+        est = [int(self.width/self.table[i][self.width-1]) for i in range(self.depth)]
         median_of_est = statistics.median(est)
-        #print(f"F0 algorithm estimation time: {time.time() - start_time}")
         return int(median_of_est)
--- streamsketchlib/rsv_sampling.py
+++ streamsketchlib/rsv_sampling.py
@@ -2,9 +2,9 @@ import random
 
 
 class RsvSampling:
-    """ Loglog algorithm: memory efficient data structure to estimate
-    the number of distinct elements in streams """
-
+    """ 
+    Sample a random element in a data stream without knowing the stream length
+    """
     def __init__(self, rsv_size):
         """ Return k random tokens (without replacement) from the stream
         """
@@ -12,18 +12,15 @@ class RsvSampling:
         self.rsv_size = rsv_size
         self.stream_length = 0
 
-
     def insert(self, token):
         """ Insert a token into the stream. """
         self.stream_length += 1
-
         if len(self.rsv) < self.rsv_size:
             self.rsv.append(token)
         else:
             j = random.randint(1, self.stream_length)
             if j <= self.rsv_size:
                 self.rsv[j-1] = token
-
 
     def reservoir(self):
         """ Return the list of sampled tokens """
