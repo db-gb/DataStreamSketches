c01b9ff8fcf4b1f6958597cea3ef63b328b2e860
db-gb <dbarnas90@gmail.com>
2023-04-24 13:21:04 -0700

    Decouple heavy hitters from count-min

--- streamsketchlib/heavy_hitters.py
+++ streamsketchlib/heavy_hitters.py
@@ -1,34 +1,51 @@
-from abc import abstractmethod
-from cm_hh_cash_register import HeavyHittersCMRegister
+from streamsketchlib.heavy_hitters_algorithms import AbstractHeavyHittersAlgorithm
+from streamsketchlib.heavy_hitters_algorithms import CountMinCashRegister
 
 
-class AbstractHeavyHittersAlgorithm:
-    @abstractmethod
-    def insert(self, token, count):
-        pass
+class HeavyHittersFinder(AbstractHeavyHittersAlgorithm):
+    COUNTMIN = 0
+    MISRAGRIES = 1
 
-    @abstractmethod
-    def get_heavy_hitters(self):
-        pass
-
-    @abstractmethod
-    def merge(self):
-        pass
-
-
-class HeavyHittersFinder:
-    COUNTMIN=0
-    MISRAGRIES=1
-
-    def __init__(self, phi=0.05, epsilon=0.2, delta=0.01, algorithm=COUNTMIN):
-        self.phi = 0.05
-        self.epsilon = 0.2
-        self.delta = 0.01
+    def __init__(self, phi=0.05, epsilon=0.2, delta=0.01, seed=42,
+                 algorithm=COUNTMIN):
+        self.phi = phi
+        self.epsilon = epsilon
+        self.delta = delta
+        self.seed = seed
+        self.algorithm = algorithm
+        self._heavy_hitters_finder = None
 
         if algorithm == HeavyHittersFinder.COUNTMIN:
-            self._heavy_hitters_finder = HeavyHittersCMRegister
-        elif algorithm ==HeavyHittersFinder.MISRAGRIES
+            self._heavy_hitters_finder = CountMinCashRegister(self.phi,
+                                                                self.epsilon,
+                                                                self.delta,
+                                                                self.seed)
+        elif algorithm == HeavyHittersFinder.MISRAGRIES:
+            pass
 
+    def insert(self, token, count=1):
+        self._heavy_hitters_finder.insert(token, count)
 
+    def get_heavy_hitters(self):
+        return self._heavy_hitters_finder.get_heavy_hitters()
 
+    def merge(self, other_finder):
+        self._heavy_hitters_finder.merge(other_finder._heavy_hitters_finder)
 
+    @classmethod
+    def from_existing(cls, original):
+        """ Creates a new sketch based on the parameters of an existing sketch.
+            Two sketches are mergeable iff they share array size and hash
+            seeds. Therefore, to create mergeable sketches, use an original to
+            create new instances. """
+        new_hh = cls()
+        new_hh.epsilon = original.epsilon
+        new_hh.delta = original.delta
+        new_hh.phi = original.phi
+        new_hh.algorithm = original.algorithm
+
+        if original.algorithm == HeavyHittersFinder.COUNTMIN:
+            new_hh._heavy_hitters_finder = CountMinCashRegister\
+                .from_existing(original._heavy_hitters_finder)
+
+        return new_hh
